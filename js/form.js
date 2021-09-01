41ed288f3e3e42d6d2b5af62d6ca7b288bb33ad7
Shounak2904 <76778946+Shounak2904@users.noreply.github.com>
2021-09-01 15:37:51 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -46,7 +46,8 @@ class Form{
 
         this.reset.mousePressed(() => {
            //make the gameState & playerCount nodes to 0 in the database
-            
+            player.updateCount(0)
+            game.update(0)
 
            //remove the player from the database
             
